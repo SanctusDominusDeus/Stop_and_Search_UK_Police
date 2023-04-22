bf490b7ae449878c338bebaa7f9266352fd607e5
Sanctus <ruben.belchior@hotmail.com>
2023-04-22 16:40:51 +0100

    go cloud

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 from flask import Flask, jsonify, request
 from peewee import (
     Model, IntegerField, FloatField,
-    TextField, IntegrityError
+    TextField, IntegrityError, BooleanField
 )
 from playhouse.shortcuts import model_to_dict
 from playhouse.db_url import connect
@@ -57,8 +57,8 @@ DB = connect(os.environ.get('DATABASE_URL') or 'sqlite:///predictions.db')
 class Prediction(Model):
     observation_id = TextField(unique=True)
     observation_data = TextField()
-    predicted_outcome = TextField()
-    actual_outcome = TextField(null=True)
+    predicted_outcome = BooleanField()
+    actual_outcome = BooleanField(null=True)
 
     class Meta:
         database = DB
@@ -85,6 +85,7 @@ with open('dtypes.pickle', 'rb') as fh:
 ########################################
 
 
+
 ########################################
 # Begin webserver stuff
 
@@ -94,74 +95,62 @@ app = Flask(__name__)
 def predict():
     
     observation = request.get_json()
-
-
-    try:
-        _id = observation['observation_id']
-    except:
-        _id = None
-        error = 'Missing observation_id.'
-        return jsonify({"observation_id":_id,"error":error})
-    
     
     #implement the mapping for the valid values
-    
     valid_category_map = {
-                "observation_id":'str()' ,
+                "observation_id":str ,
                 "Type":['Person search','Person and Vehicle search','Vehicle search'],
-                'Date':'()str',
+                'Date':str,
                 'Part of a policing operation':[True, False],
-                'Latitude':'float()',#try to add range for the uk
-                'Longitude':'float()',#try to add range for the uk
+                'Latitude':float,#try to add range for the uk
+                'Longitude':float,#try to add range for the uk
                 'Gender': ['Male','Female','Other'],
                 'Age range':['under 10','10-17','18-24','25-34','over 34'],
                 'Officer-defined ethnicity':['White','Asian','Black','Other','Mixed'],
-                'Legislation':'str()',
-                'Object of search':'str()',#should it be a list???
-                'station':'str()'
+                'Legislation':str,
+                'Object of search':str,#should it be a list???
+                'station':str
 
     }
-    #to ask for if it has all inputs
-    for key in valid_category_map.keys():
-        if key not in observation.keys():
+
+    # the followig is to verify the range
+    _id = None
+    for key, valid_categories in valid_category_map.items():
+        if key in observation.keys():
+            _id = observation['observation_id']
+            value = observation[key]
+            #checking only format
+            if type(valid_categories)!=list:
+                if type(value)==valid_categories:
+                    continue
+                else:
+                    error = "Invalid format provided for {}. Please provide a {}".format(key,valid_categories)
+                    return jsonify({"observation_id":_id,"error":error})
+
+            #checking format and values
+            elif value not in valid_categories:
+                error = "Invalid value provided for {}: {}. Allowed values are: {}".format(
+                    key, value, ",".join(["'{}'".format(v) for v in valid_categories]))
+                return jsonify({"observation_id":_id,"error":error})
+        else:
             error = '{} is not provided.'.format(key)
             return jsonify({"observation_id":_id,"error":error})
+    
     #not run with more than expected inputs
     for key in observation.keys():
         if key not in valid_category_map.keys():
             error = '{} is not a valid input.'.format(key)
             return jsonify({"observation_id":_id,"error":error})
-    
-    
-    # the followig is to verify the range
-    ''' 
-    for key, valid_categories in valid_category_map.items():
-        if key in observation:
-            value = observation[key]
-            if value not in valid_categories:
-                error = "Invalid value provided for {}: {}. Allowed values are: {}".format(
-                    key, value, ",".join(["'{}'".format(v) for v in valid_categories]))
-                return jsonify({"observation_id":_id,"error":error})
-        else:
-            error = "Categorical field {} missing".format(key)
-            return jsonify({"observation_id":_id,"error":error})
-    '''
-    
+
+
     obs = pd.DataFrame([observation], columns=columns).astype(dtypes)
     prediction = pipeline.predict(obs)[0]
     #proba = pipeline.predict_proba(obs)[0,1]
 
     response = dict()
-    #response['observation_id'] = _id
     response['outcome'] = bool(prediction)
-    #response['prediction'] = bool(prediction)
-    #response['probability'] = proba
     p = Prediction(
         observation_id=_id,
-        #proba=proba,
-        #observation=request.data
-        
-        #observation_data=jsonify(observation),
         observation_data=observation,
         predicted_outcome = prediction
     )
@@ -169,7 +158,7 @@ def predict():
         print(observation)
         p.save()
     except IntegrityError:
-        error_msg = 'Admission ID: "{}" already exists'.format(_id)
+        error_msg = 'Admission ID: {} already exists'.format(_id)
         response['error'] = error_msg
         print(error_msg)
         DB.rollback()
@@ -179,6 +168,41 @@ def predict():
 @app.route('/search_result/', methods=['POST'])
 def update():
     obs = request.get_json()
+
+    # the followig is to verify the range => it is stupid to repeat this code but I cannot implemented inside a function that works for both cases
+    #implement the mapping for the valid values
+    validinput_category_map = {
+                "observation_id":str ,
+                "outcome":[True, False]
+    }
+    _id = None
+    for key, valid_categories in validinput_category_map.items():
+        if key in obs.keys():
+            _id = obs['observation_id']
+            value = obs[key]
+            #checking only format
+            if type(valid_categories)!=list:
+                if type(value)==valid_categories:
+                    continue
+                else:
+                    error = "Invalid format provided for {}. Please provide a {}".format(key,valid_categories)
+                    return jsonify({"observation_id":_id,"error":error})
+
+            #checking format and values
+            elif value not in valid_categories:
+                error = "Invalid value provided for {}: {}. Allowed values are: {}".format(
+                    key, value, ",".join(["'{}'".format(v) for v in valid_categories]))
+                return jsonify({"observation_id":_id,"error":error})
+        else:
+            error = '{} is not provided.'.format(key)
+            return jsonify({"observation_id":_id,"error":error})
+    
+    #not run with more than expected inputs
+    for key in obs.keys():
+        if key not in validinput_category_map.keys():
+            error = '{} is not a valid input.'.format(key)
+            return jsonify({"observation_id":_id,"error":error})
+
     try:
         p = Prediction.get(Prediction.observation_id == obs['observation_id'])
         p.actual_outcome = obs['outcome']
@@ -186,10 +210,14 @@ def update():
         ret_dict = model_to_dict(p)
         k = {'observation_id':ret_dict['observation_id'],'outcome':ret_dict['actual_outcome'],'predicted_outcome':ret_dict['predicted_outcome']}
         return jsonify(k)
-        #return jsonify(model_to_dict(p))
     except Prediction.DoesNotExist:
-        error_msg = 'Observation ID: "{}" does not exist'.format(obs['admission_id'])
+        error_msg = 'Observation ID: "{}" does not exist'.format(obs['observation_id'])
         return jsonify({'error': error_msg})
+
+
+
+
+
 
 @app.route('/list-db-contents')
 def list_db_contents():
